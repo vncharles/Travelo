566d8ec01e99fb2ba954fbf5bce68d0c0e013bbd
vncharles <quoctrong.13022002@gmail.com>
2023-10-05 18:51:00 +0700

    create base source up to repo

---
+++ src/main/java/com/fit/Travelo/TraveloApplication.java
@@ -0,0 +1,13 @@
+package com.fit.Travelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveloApplication.class, args);
+	}
+
+}
