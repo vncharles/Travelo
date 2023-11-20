a72f58c0b9c816d0bcbf99e4077d3e8078a9d242
vncharles <quoctrong.13022002@gmail.com>
2023-11-20 18:06:04 +0700

    oke

--- src/main/java/com/fit/Travelo/config/CorsConfig.java
+++ src/main/java/com/fit/Travelo/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION");
     }
 }
