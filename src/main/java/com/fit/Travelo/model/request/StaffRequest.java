bdf405ef44a1716d1fcbaeb34b4d1afc5fd96ff8
vncharles <quoctrong.13022002@gmail.com>
2023-11-20 21:47:10 +0700

    fix cors

--- src/main/java/com/fit/Travelo/model/request/StaffRequest.java
+++ src/main/java/com/fit/Travelo/model/request/StaffRequest.java
@@ -4,19 +4,20 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 
-@Data
+@Data @ToString @AllArgsConstructor
 public class StaffRequest {
-    @Size(min = 1, max = 100, message = "Full name must be between 1 and 100 characters")
-    @NotBlank(message = "Full name is required")
-    @JsonAlias("full_name")
-    @Pattern(regexp = "^[a-zA-Z ]+$", message = "Input must contain only alphabetic characters (A-Z, a-z) and spaces.")
+//    @Size(min = 1, max = 100, message = "Full name must be between 1 and 100 characters")
+//    @NotBlank(message = "Full name is required")
+//    @Pattern(regexp = "^[a-zA-Z ]+$", message = "Input must contain only alphabetic characters (A-Z, a-z) and spaces.")
     private String name;
 
-    @Pattern(regexp = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Email is invalid")
-    @NotBlank(message = "Email is required")
+//    @Pattern(regexp = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Email is invalid")
+//    @NotBlank(message = "Email is required")
     private String email;
     private String phone;
     private String personId;
--- src/main/java/com/fit/Travelo/security/SecurityConfiguration.java
+++ src/main/java/com/fit/Travelo/security/SecurityConfiguration.java
@@ -15,6 +15,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -31,8 +34,36 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)
             throws Exception {
-        http.cors(cors -> cors.disable());
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:5500"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+
+//        http.cors().configurationSource(request -> corsConfiguration).and().csrf().disable()
+//                .authorizeRequests(request->request
+//                    .requestMatchers("/reset-password").hasAnyAuthority(ERole.ROLE_ADMIN.name(), ERole.ROLE_STAFF.name(), ERole.ROLE_USER.name())
+//                    .requestMatchers(HttpMethod.GET, "/staff/{id}").hasAnyAuthority(ERole.ROLE_ADMIN.name(), ERole.ROLE_STAFF.name())
+//                    .requestMatchers("/staff/**").hasAnyAuthority(ERole.ROLE_ADMIN.name())
+//                    .requestMatchers(HttpMethod.GET, "/booking/list-by-email").permitAll()
+//                    .requestMatchers(HttpMethod.POST, "/booking").permitAll()
+//                    .requestMatchers("/booking/**").hasAnyAuthority(ERole.ROLE_STAFF.name(), ERole.ROLE_ADMIN.name())
+//                    .requestMatchers(HttpMethod.GET, "/customer/detail-person").hasAnyAuthority(ERole.ROLE_STAFF.name(), ERole.ROLE_ADMIN.name())
+//                    .requestMatchers("/customer/**").hasAnyAuthority(ERole.ROLE_STAFF.name(), ERole.ROLE_ADMIN.name())
+//                    .requestMatchers(HttpMethod.GET, "/location", "/location/{id}").permitAll()
+//                    .requestMatchers("/location/**").hasAnyAuthority(ERole.ROLE_STAFF.name(), ERole.ROLE_ADMIN.name())
+//                    .requestMatchers(HttpMethod.GET, "/tour", "/tour/{id}").permitAll()
+//                    .requestMatchers("/tour/**").hasAnyAuthority(ERole.ROLE_STAFF.name(), ERole.ROLE_ADMIN.name())
+//                    .requestMatchers("/tour-info/**").hasAnyAuthority(ERole.ROLE_STAFF.name(), ERole.ROLE_ADMIN.name())
+//                    .requestMatchers("/**").permitAll()
+//               .anyRequest().authenticated()
+//                ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.cors().configurationSource(request -> corsConfiguration);
         http.csrf(csrf -> csrf.disable());
+
         http.authorizeHttpRequests(request -> request
                 .requestMatchers("/reset-password").hasAnyAuthority(ERole.ROLE_ADMIN.name(), ERole.ROLE_STAFF.name(), ERole.ROLE_USER.name())
                 .requestMatchers(HttpMethod.GET, "/staff/{id}").hasAnyAuthority(ERole.ROLE_ADMIN.name(), ERole.ROLE_STAFF.name())
