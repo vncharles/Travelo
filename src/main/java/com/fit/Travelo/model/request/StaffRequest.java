80199a98c7aab08ef07aadb821fec4a3c26e907f
vncharles <quoctrong.13022002@gmail.com>
2023-11-21 10:50:10 +0700

    fix conflit

--- src/main/java/com/fit/Travelo/model/request/StaffRequest.java
+++ src/main/java/com/fit/Travelo/model/request/StaffRequest.java
@@ -1,16 +1,19 @@
 package com.fit.Travelo.model.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
-
-
-@Data @AllArgsConstructor @NoArgsConstructor
+@Data
 public class StaffRequest {
+    @Size(min = 1, max = 100, message = "Full name must be between 1 and 100 characters")
+    @NotBlank(message = "Full name is required")
     private String name;
+
+    @Pattern(regexp = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Email is invalid")
+    @NotBlank(message = "Email is required")
     private String email;
     private String phone;
     private String personId;
